88ed85b3fc4c36ef8a89747544f572c2d34fc21c
Shubham0209 <shubhamp.singh@yahoo.com>
2018-04-27 19:36:58 +0530

    Update ClassifyNB.py

--- ClassifyNB.py
+++ ClassifyNB.py
@@ -11,3 +11,12 @@ def classify(features_train, labels_train):
     clf = GaussianNB()
     clf.fit(features_train, labels_train)
     return clf;
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+   
+    from sklearn.naive_bayes import GaussianNB
+     clf = GaussianNB()#TODO
+     clf.fit(features_train, labels_train)
+     pred = clf.predict(features_test)#TODO
+    accuracy = clf.score(features_test, labels_test)
+    return accuracy;
